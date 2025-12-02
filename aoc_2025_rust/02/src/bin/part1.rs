7eb6803cd4b8e36c100d793b214d650ff0461cd6
Newtoneiro <latosek.bar@gmail.com>
2025-12-02 12:58:20 +0100

    finish day 2

--- aoc_2025_rust/02/src/bin/part1.rs
+++ aoc_2025_rust/02/src/bin/part1.rs
@@ -1,3 +1,34 @@
+use day_02::utils::read_lines;
+use std::collections::HashSet;
+
 fn main() {
-    println!("part1");
+    let input = &read_lines("src/input.txt")[0];
+    let ranges = input.split(",");
+
+    let mut invalid_ids: HashSet<u64> = HashSet::new();
+    for range in ranges {
+        let parts: Vec<&str> = range.split('-').collect();
+        let low: u64 = parts[0].parse().unwrap();
+        let high: u64 = parts[1].parse().unwrap();
+        
+        let low_digits = low.to_string().len();
+        let high_digits = high.to_string().len();
+
+        for i in low_digits..=high_digits {
+            if i % 2 == 0 {
+                let n: u32 = (i / 2).try_into().unwrap();
+                let bottom = 10u64.pow(n - 1);
+                let upper = 10u64.pow(n) - 1;
+                for digit_part in bottom..=upper {
+                    let full_digit: u64 = format!("{}{}", digit_part, digit_part)
+                        .parse::<u64>()
+                        .unwrap();
+                    if full_digit >= low && full_digit <= high {
+                        invalid_ids.insert(full_digit);
+                    } 
+                }
+            }
+        }
+    }
+    println!("{}", invalid_ids.iter().sum::<u64>());
 }
--- aoc_2025_rust/02/src/bin/part2.rs
+++ aoc_2025_rust/02/src/bin/part2.rs
@@ -1,3 +1,38 @@
+use day_02::utils::read_lines;
+use std::collections::HashSet;
+
+fn proper_divisors(n: usize) -> Vec<usize> {
+    (1..n)
+        .filter(|i| n % i == 0)
+        .collect()
+}
+
 fn main() {
-    println!("part2");
+    let input = &read_lines("src/input.txt")[0];
+    let ranges = input.split(",");
+
+    let mut invalid_ids: HashSet<u64> = HashSet::new();
+    for range in ranges {
+        let parts: Vec<&str> = range.split('-').collect();
+        let low: u64 = parts[0].parse().unwrap();
+        let high: u64 = parts[1].parse().unwrap();
+        
+        let low_digits = low.to_string().len();
+        let high_digits = high.to_string().len();
+
+        for i in low_digits..=high_digits {
+            for divisor in proper_divisors(i) {
+                let bottom = 10u64.pow(divisor as u32 - 1);
+                let upper = 10u64.pow(divisor as u32) - 1;
+                for digit_part in bottom..=upper {
+                    let full_digit_str = digit_part.to_string().repeat((i / divisor) as usize);
+                    let full_digit: u64 = full_digit_str.parse().unwrap(); 
+                    if full_digit >= low && full_digit <= high {
+                        invalid_ids.insert(full_digit);
+                    } 
+                }
+            }
+        }
+    }
+    println!("{}", invalid_ids.iter().sum::<u64>());
 }
